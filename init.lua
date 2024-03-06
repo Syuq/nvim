42a6c6ae4e5041cc3788e0d8eb11c751cdc80ee0
Syuq <tatd.wine@gmail.com>
2024-03-06 16:40:40 +0700

    🚀 feat(neovide): config

--- init.lua
+++ init.lua
@@ -1,13 +1,16 @@
-if vim.loader then
-	vim.loader.enable()
+-- Load Modules:
+require("core")
+
+-- Helper function for transparency formatting
+local alpha = function()
+  return string.format("%x", math.floor(255 * vim.g.transparency or 0.8))
 end
+-- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
+vim.g.neovide_transparency = 0.6
+vim.g.transparency = 0.8
+vim.g.neovide_background_color = "#0f1117" .. alpha()
 
-_G.dd = function(...)
-	require("util.debug").dump(...)
-end
-vim.print = _G.dd
+vim.g.neovide_cursor_animate_in_insert_mode = true
+vim.g.neovide_cursor_vfx_mode = "torpedo"
 
-require("config.lazy")
-
-vim.opt.guifont = { "JetBrainsMono Nerd Font", ":h9" }
-
+vim.o.guifont = "JetBrainsMono Nerd Font:h9:#h-slight"
